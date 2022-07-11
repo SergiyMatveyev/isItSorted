9167221148c6ed0003aa19e585bb171fb51f60f5
sergiy.matveyev <sergiy.matveyev@vakoms.com>
2022-07-12 01:46:37 +0300

    Modefied tests

--- index.js
+++ index.js
@@ -28,19 +28,18 @@ function isSorted(array, way, start) {
 // Tests
 function test() {
   let testArray = [
-    [1, 0, 30, 20, 11, 16],
-    [0, 0, 0, 1, 2, 3],
-    [1, 2, 3, 4, 5, 6],
-    [9, 8, 7, 6, 5, 4],
-    [10, 10, 10, 3, 2, 1],
-    [3, 2, 1, 0, 0, 0],
-    [1, 2, 3, 5, 5, 5],
-    [1, 2, 5, 5, 5, 6],
-    [1, 0, 30, 20, 11, 16],
+    [[0, 0, 0, 1, 2, 3], "Yes, ASC"],
+    [[1, 2, 3, 4, 5, 6], "Yes, ASC"],
+    [[1, 2, 3, 5, 5, 5], "Yes, ASC"],
+    [[1, 2, 5, 5, 5, 6], "Yes, ASC"],
+    [[9, 8, 7, 6, 5, 4], "Yes, DESC"],
+    [[10, 10, 10, 3, 2, 1], "Yes, DESC"],
+    [[3, 2, 1, 0, 0, 0], "Yes, DESC"],
+    [[1, 0, 30, 20, 11, 16], "No"],
   ];
 
   testArray.forEach((element) => {
-    console.log(checkIsArraySorted(element));
+    console.log(checkIsArraySorted(element[0]) === element[1]);
   });
 }
 
